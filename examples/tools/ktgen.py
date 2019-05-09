110dad0413bceb89e04393c1d88ee68c321f0cfd
Tom Phan <ppngiap@gmail.com>
2019-05-09 10:44:08 -0700

    Output java command

---
+++ examples/tools/ktgen.py
@@ -0,0 +1,42 @@
+import sys
+import os
+import stat
+
+
+def mkdir_if_need_to(dir):
+    if not os.path.isdir(dir):
+        os.mkdir(dir)
+
+def gen_file_if_need_to(dirname, name):
+    filepath = os.path.join(dirname, name + ".kt");
+    if not os.path.isfile(filepath):
+        f = open(filepath, "w")
+
+def gen_compile_if_need_to(dirname, name):
+    filepath = os.path.join(dirname, "compile")
+    if not os.path.isfile(filepath):
+        f = open(filepath, "w")
+        f.write("kotlinc ./%s.kt -include-runtime -d %s.jar\n" % (name, name))
+        f.close()
+    os.chmod(filepath, stat.S_IRUSR | stat.S_IWUSR | stat.S_IEXEC)
+
+def gen_run_if_need_to(dirname, name):
+    filepath = os.path.join(dirname, "run")
+    if not os.path.isfile(filepath):
+        f = open(filepath, "w")
+        f.write("java -jar %s.jar\n" % name)
+        f.close()
+    os.chmod(filepath, stat.S_IRUSR | stat.S_IWUSR | stat.S_IEXEC)
+
+
+def main():
+    name = sys.argv[1]
+    dirname = name.lower()
+    mkdir_if_need_to(dirname)
+    gen_file_if_need_to(dirname, name)
+    gen_compile_if_need_to(dirname, name)
+    gen_run_if_need_to(dirname, name);
+
+
+if __name__ == "__main__":
+    main()
---
+++ examples/tools/ktgen.sh
@@ -0,0 +1,1 @@
+python tools/ktgen.py $1
